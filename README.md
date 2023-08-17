251151629e49801b0fbfbe9f56a3b6398d6ac306
Sean Stocker <sean.stocker14@gmail.com>
2023-08-17 20:51:03 +0100

    feat: created server and html page with form for submitting (and receiving) messages

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node.js Real-Time Chat App
+
+This Node.js Real-Time Chat App is a simple yet powerful demonstration of Node.js's event-driven architecture. This application enables users to engage in real-time conversations across multiple browser tabs or devices. Leveraging the socket.io library, the app establishes seamless communication between the server and clients through event-driven interactions.
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
